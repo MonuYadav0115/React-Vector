aef796d34e5ad278072d759d98c3f9aa12f1ff70
Monu Yadav <monuy7883@gmail.com>
2026-01-31 11:11:54 +0530

    react

---
+++ 13project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  return (
+    <div>{time.toLocaleTimeString()}</div>
+  )
+}
+
+export default App
